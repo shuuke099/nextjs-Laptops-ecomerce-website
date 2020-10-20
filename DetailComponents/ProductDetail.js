504dfed2eabc2e536db7e2957a19dc4194a59b25
shuukeyare099 <koryaal6@gmail.com>
2020-10-19 22:39:54 -0700

    organized some files

--- DetailComponents/ProductDetail.js
+++ DetailComponents/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { colors } from '../utilits/styles';
 import { FiHeart } from 'react-icons/fi';
